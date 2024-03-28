6fb925f2948f92a89964912cf5204f17b8a98810
M Awais <awais23112001@gmail.com>
2024-03-28 10:24:20 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// practising Operators
+var a = 10;
+var b = 5;
+var c = a++ + ++a + ++b + b++ + b + a-- + b-- + a;
+console.log(c);
---
+++ main.ts
@@ -0,0 +1,7 @@
+// practising Operators
+
+let a = 10
+let b = 5
+let c = a++ + ++a + ++b + b++ + b + a-- + b-- + a
+
+console.log(c)
